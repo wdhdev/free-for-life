5d78984e5eacd50b33879999e114e597106e9aca
William Harrison <william@williamdavidharrison.com.au>
2023-06-03 08:37:15 +0800

    feat: exclude `_` files + add comment for each file

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -17,13 +17,26 @@ while IFS= read -r line; do
     echo "$line" >> "$tmp_file"
 done < README.md
 
-for file in $files; do
-    echo "Adding contents of $file..."
-    cat "data/$file" >> "$tmp_file"
+if [[ -f "data/_start.md" ]]; then
+    echo "Adding contents of _start.md..."
+    cat "data/_start.md" >> "$tmp_file"
+fi
 
-    if [[ "$file" != $(echo "${files}" | tail -n 1) ]]; then
-        echo "" >> "$tmp_file"
+for file in $files; do
+    if [[ "$file" == _* ]]; then
+        continue
     fi
+
+    echo "Adding contents of $file..."
+    echo "" >> "$tmp_file"
+    echo "<!-- $file -->" >> "$tmp_file"
+    cat "data/$file" >> "$tmp_file"
 done
+
+if [[ -f "data/_end.md" ]]; then
+    echo "Adding contents of _end.md..."
+    echo "" >> "$tmp_file"
+    cat "data/_end.md" >> "$tmp_file"
+fi
 
 mv "$tmp_file" README.md
